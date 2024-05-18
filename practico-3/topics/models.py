7a0623fe675e8d655008d4b93387e6a768e51c6f
Guillermo Rojas <apemangr@gmail.com>
2024-05-18 13:51:34 -0400

    Modelo de tópico modificado

--- practico-3/topics/models.py
+++ practico-3/topics/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Topic(models.Model):
+    title=  models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now = True)
+    user = models.TextField()
+
+class Reply (models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    user = models.TextField()
